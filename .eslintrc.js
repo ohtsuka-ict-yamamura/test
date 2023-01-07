9a19fa5e2715f7d020c6bbeae5d8face4891b45a
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-01-07 15:59:19 +0900

    chore: configure ESLint
    In this project, it assumes that we well use TypeScript, so write ESLint
    config for TypeScript. Also, add some rules to manage imports because
    both sorting out import orders and deleting unnecessary imports are repeated
    operation.

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint", "import", "unused-imports"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "next/core-web-vitals",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        argsIgnorePattern: "_",
+        ignoreRestSiblings: false,
+        varsIgnorePattern: "_",
+      },
+    ],
+    "unused-imports/no-unused-imports": "warn",
+    "import/newline-after-import": "warn",
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "type",
+          "object",
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        pathGroups: [
+          {
+            pattern: "@/lib/**",
+            group: "internal",
+            position: "before",
+          },
+          {
+            pattern: "@/utils/**",
+            group: "internal",
+            position: "before",
+          },
+          {
+            pattern: "@/hooks/**",
+            group: "internal",
+            position: "before",
+          },
+          {
+            pattern: "@/components/**",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@/types/**",
+            group: "type",
+            position: "after",
+          },
+        ],
+      },
+    ],
+  },
+};
