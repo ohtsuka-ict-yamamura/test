a0aa35c4bdf2b11dcb89cb589c2783ff189c6746
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-01-11 07:04:04 +0900

    fix: update `_document.tsx` to initial version
    Since I broke this file in when I sorted out some files and code, I
    fixed it anyway.
    
    ref: issue #2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,13 @@
-const Document = () => <></>;
+import { Html, Head, Main, NextScript } from 'next/document';
 
-export default Document;
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
